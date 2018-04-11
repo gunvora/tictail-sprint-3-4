8dd9b6ebafd2a4a65126a55f05c0380b6eb270e8
Freja Busby <frejabusby@gmail.com>
2018-04-11 17:22:04 +0200

    responsive + hover effects

--- code/src/components/product.js
+++ code/src/components/product.js
@@ -11,7 +11,6 @@ class Product extends React.Component {
         <img className="product-image" src={this.props.product.images[0].url} alt={this.props.product.title + " " + this.props.product.categories[0].title} />
         <h2 className="product-title">{this.props.product.title}</h2>
         <h2 className="product-price">{this.props.product.price / 100} kr</h2>
-        <button className="buy-button">Add to bag</button>
       </Link>
       </div>
     )
