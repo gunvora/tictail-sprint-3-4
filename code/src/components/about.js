e7faa0e7b1c0288db501b015f22e76fe87ee9b2b
Davidlicona <davidlicona@users.noreply.github.com>
2018-04-11 14:17:24 +0200

    News/About

--- code/src/components/about.js
+++ code/src/components/about.js
@@ -1,10 +1,15 @@
 import React from "react"
+import "./app.css"
+import "./about.css"
 
 class About extends React.Component {
   render() {
     return (
-      <div className="about">
-        <h1>ABOUT</h1>
+      <div className="main-content">
+        <div className="about">
+          <h1>Lunacy</h1>
+          <p>About Lunacy blah blah blah blah blah blah</p>
+        </div>
       </div>
     )
   }
--- code/src/components/news.js
+++ code/src/components/news.js
@@ -1,11 +1,15 @@
 import React from "react"
 import "./news.css"
+import "./app.css"
 
 class News extends React.Component {
   render() {
     return (
-      <div className="news">
-        <h1>News</h1>
+      <div className="main-content">
+        <div className="news">
+          <h1>News</h1>
+          <p>About Lunacy blah blah blah blah blah blah</p>
+        </div>
       </div>
     )
   }
