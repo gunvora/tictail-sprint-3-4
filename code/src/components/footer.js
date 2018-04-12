c3ec8eb31e0361d541b6f6342452cb10fabf260f
Freja Busby <frejabusby@gmail.com>
2018-04-12 21:24:24 +0200

    Changes Facebbok link to desktop one

--- code/src/components/footer.js
+++ code/src/components/footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <div className="footer-icon"><a href="https://m.facebook.com/LNCYIP/" target="_blank"><img src="../images/facebook.png" /></a></div>
+        <div className="footer-icon"><a href="https://facebook.com/LNCYIP/" target="_blank"><img src="../images/facebook.png" /></a></div>
         <div className="footer-icon"><a href="https://www.instagram.com/lncyip/" target="_blank"><img src="../images/instagram.png" /></a></div>
         <div className="footer-icon"><a href="mailto:teamyellow@technigo.io"><img src="../images/email-icon.png" /></a></div>
       </div>
