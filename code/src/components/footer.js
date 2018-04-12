bea7587162f878ef3db0747aee18185b914ac949
Davidlicona <davidlicona@users.noreply.github.com>
2018-04-12 09:47:28 +0200

    Header/Footer

--- code/src/components/footer.js
+++ code/src/components/footer.js
@@ -5,9 +5,9 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <div><a href="https://m.facebook.com/LNCYIP/" target="_blank">Facebook</a></div>
-        <div><a href="https://www.instagram.com/lncyip/" target="_blank">Instagram</a></div>
-        <div><a href="mailto:teamyellow@technigo.io">Email</a></div>
+        <div className="footer-icon"><a href="https://m.facebook.com/LNCYIP/" target="_blank"><img src="../images/facebook.png" /></a></div>
+        <div className="footer-icon"><a href="https://www.instagram.com/lncyip/" target="_blank"><img src="../images/instagram.png" /></a></div>
+        <div className="footer-icon"><a href="mailto:teamyellow@technigo.io"><img src="../images/email-icon.png" /></a></div>
       </div>
     )
   }
