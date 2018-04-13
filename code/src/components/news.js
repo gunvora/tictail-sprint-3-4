854da12c9ab4d7f7271fb5aaf5c9e697bc0dde24
Davidlicona <davidlicona@users.noreply.github.com>
2018-04-13 11:36:26 +0200

    News/Hector photo

--- code/src/components/news.js
+++ code/src/components/news.js
@@ -10,10 +10,10 @@ class News extends React.Component {
         <div className="content-box news">
           <div className="news-section-left">
             <p className="created-date">2018-04-12</p>
-            <h2>28 April</h2>
-            <h3>Release party</h3>
-            <p>About Lunacy blah blah blah blah blah blah</p>
-            <a href="">Lorem</a>
+            <h2>Pop-up Store!</h2>
+            <h3>Stockholm</h3>
+            <p>Join us at <a href="http://www.snickarsrecords.com/">Snickars Records</a> on Saturday the 28th of April. We will be playing our favorite Records
+            and </p>
           </div>
           <div className="news-section-right">
             <img src="./images/lunacy-news-img-1.png" />
