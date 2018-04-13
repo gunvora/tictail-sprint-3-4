3718c1d10c63e26e9415f1cad3c1586438d450a2
Davidlicona <davidlicona@users.noreply.github.com>
2018-04-13 11:25:57 +0200

    About text

--- code/src/components/about.js
+++ code/src/components/about.js
@@ -6,9 +6,17 @@ class About extends React.Component {
   render() {
     return (
       <div className="main-content">
-        <div className="about">
-          <h1>Lunacy</h1>
-          <p>About Lunacy blah blah blah blah blah blah</p>
+      <h1>About</h1>
+        <div className="content-box about">
+          <div className="about-text">
+            <p>Lunacy is a streetwear brand created in the summer of 2017 by Héctor Álvarez Zepeda, a Mexican illustrator/painter and DJ that is know in the electronic music community as TRR. His work as a designer of promotional material for music events allowed him to collaborate with various electronic music artists, including Dutch DJ/producer Legowelt, who launched his own fashion label using an illustration made by Héctor.</p>
+            <p>After these collaborations TRR decided to launch his own brand, in order to have a platform to display his own work as an illustrator. The brand takes inspiration from mental disorders, trying to make graphic statements that work as a reminder of the fragility of mental health in our fast-paced world.</p>
+            <p>Lunacy is still a very small brand, a collaboration between TRR and some of this closest friends. After only a few months, the brand has managed to set a foot inside the Mexican electronic music community and now aims to expand to other territories.</p>
+          </div>
+          <div className="about-image">
+            <img src="../images/trr.jpg" alt="Héctor Álvarez Zepeda" />
+            <img src="../images/terr.jpg" alt="Héctor Álvarez Zepeda" />
+          </div>
         </div>
       </div>
     )
